21b6835e15a6c2b04b29fb546a3b7d7dad5c5235
phamhoa6991 <82876671+phamhoa6991@users.noreply.github.com>
2021-11-23 23:44:37 +0700

    #update main

--- App.js
+++ App.js
@@ -25,32 +25,6 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -60,7 +34,7 @@ const App = () => {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-     <Text>helooo</Text>
+     <Text>gawjgao </Text>
     </SafeAreaView>
   );
 };
