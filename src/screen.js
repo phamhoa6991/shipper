82304fc2564b5ed2f26d0e7e7ede5919e9c140b8
phamhoa6991 <82876671+phamhoa6991@users.noreply.github.com>
2021-11-26 00:03:45 +0700

    #update screen

--- src/screen.js
+++ src/screen.js
@@ -1,40 +1,68 @@
 import React from "react";
-import { View,Image, Text,StyleSheet,Button,TextInput  } from 'react-native';
+import { View, Image, Text, StyleSheet, Button, TextInput, TouchableO, TouchableOpacity, } from 'react-native';
 
 function Screen() {
     return (
         <View style={styles.container}>
-            <Image source={require('./img/ic_logo.png')}style={styles.img}/>
-            <Text style={styles.text}>ĐĂNG NHẬP</Text>
-            <TextInput placeholder='Tên đăng nhập' style={styles.input}/>
-            <TextInput placeholder='Mật khẩu' style={styles.input}/>
+            <View style={styles.img}>
+                <Image source={require('./img/ic_logo.png')} />
+            </View>
+            <View >
+                <Text style={styles.text}>ĐĂNG NHẬP</Text>
+                <TextInput placeholder='Tên đăng nhập' style={styles.input} />
+                <TextInput placeholder='Mật khẩu' style={styles.input} />
+            </View>
+            <View style={styles.login}>
+                <Text>Đổi mật khẩu</Text>
+                <TouchableOpacity >
+                    <Image style={styles.imageButtonStyle} source={require('./img/ic_next.png')}></Image>
+
+                </TouchableOpacity>
+            </View>
         </View>
-        );
+    );
 }
 const styles = StyleSheet.create({
     container: {
-        display:'flex',
-        flexDirection:'column',
+        display: 'flex',
+        flexDirection: 'column',
+    },
 
-         alignItems:'center',
-        justifyContent:'center',
-    },
-    text:{
+    text: {
+        fontSize: 25,
         fontWeight: 'bold',
-        color:'#151516',
-        display:'flex',
-        justifyContent:'flex-start',
-        alignItems:'flex-start'
+        color: '#151516',
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        marginLeft: 36
     },
-    img:{
-        marginTop:100,
-        marginBottom:95,
-        display:'flex',
-        alignItems:'center',
-        justifyContent:'center',
+    img: {
+        marginTop: 100,
+        marginBottom: 95,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-    input :{
+    input: {
+        display: 'flex',
+        marginLeft: 35,
+    },
+    login: {
+        display: "flex",
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'baseline',
+        marginLeft: 35,
+        marginRight: 37,
+        marginTop: 121,
+    },
+    imageButtonStyle:{
+        width:51,
+        height:51,
+
+    },
         
-    }
+    
 })
 export default Screen;
